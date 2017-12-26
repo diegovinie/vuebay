e562c37ebffec266f04d880518b69dfa7029e166
Diego Viniegra <diego.viniegra@gmail.com>
2017-12-26 15:37:03 -0400

    creando NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-menu theme="dark" class="el-menu-demo" mode="horizontal">
+        <el-menu-item index="1">VueBay</el-menu-item>
+        <el-menu-item index="2">
+            Buscar productos: <input type="text" v-model="query">
+            <el-button size="mini" @click="doSearch(query)">Buscar</el-button>
+        </el-menu-item>
+        
+    </el-menu>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                query: ''
+            }
+        },
+        methods: {
+            doSearch(query){
+                console.log(query);
+                
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/es'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import NavBar from './components/NavBar'
+
+Vue.use(element, {locale})
+
+Vue.component('nav-bar', NavBar)
 
 new Vue({
   el: '#app',
