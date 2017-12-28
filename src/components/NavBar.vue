d48908a96a6893bb25023a6a8a6f53093774aa75
Diego Viniegra <diego.viniegra@gmail.com>
2017-12-28 19:04:15 -0400

    lista de lugares

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,6 +2,7 @@
     <el-menu theme="dark" class="el-menu-demo" mode="horizontal"  background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
         <el-menu-item index="1">VueBay</el-menu-item>
         <el-menu-item index="2">
+            Buscar productos
         </el-menu-item>
     </el-menu>
 </template>
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -4,6 +4,12 @@
       <el-select v-model="category">
           <el-option v-for="cat in categories" :key="cat.id" :label="cat.name" :value="cat.id"></el-option>
       </el-select>
+      <el-card>
+          <div slot="header" class="clearfix">Lugares:</div>
+          <div class="text item" v-for="loc in locations" :key="loc.id">
+              <el-badge :value="loc.quantity" class="item"><el-button type="text">{{ loc.state_name }}</el-button></el-badge>
+          </div>
+      </el-card>
   </div>
 </template>
 
@@ -20,6 +26,12 @@
         computed: {
             categories: function(){
                 return store.state.categories
+            },
+            list: function(){
+                return store.state.list
+            },
+            locations(){
+                return this.getLocations(this.list)
             }
         },
         methods: {
@@ -28,6 +40,47 @@
                 .then(res =>{
                     store.commit('updateCategories', res)
                 })
+            },
+            
+            /** 
+             * Devuelve una lista de productos organizados por estado
+             */
+            getLocations(list){
+ 
+                const locations = {
+                    statesList: [],
+                    states: []
+                }
+
+                // Revisa cada producto si está en statesList y lo añade a states
+                list.forEach(function(item){
+                    
+                    // Si no hay nada en la búsqueda 
+                    if(!item.address){
+                        return null
+                    }
+
+                    let itemState = item.address.state_id
+
+                    if(locations.statesList.indexOf(itemState) === -1){
+                        locations.statesList.push(itemState)
+                        locations.states.push({
+                            state_id: itemState,
+                            state_name: item.address.state_name,
+                            quantity: 1,
+                            products: [item]
+                        })   
+                    }
+                    else{
+                        let a = locations.states.find(function(e){
+                            return e.state_id == itemState
+                        })                       
+                        a.products.push(item)
+                        a.quantity++
+                    }
+                })
+                // Retorna sólo el array de estados
+                return locations.states              
             }
         },
         created(){
